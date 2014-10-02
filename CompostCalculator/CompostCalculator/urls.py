affc2369332356d83139e2a07794d3958f3276fe
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-10-02 12:14:48 -0700

    sortof overrode the account/register url to use my new registration view, that overrode the get_success_url

--- CompostCalculator/CompostCalculator/urls.py
+++ CompostCalculator/CompostCalculator/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from calculator.views import RegistrationView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'CompostCalculator.views.home', name='home'),
     url(r'^calculator/', include('calculator.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('registration.auth_urls')),
     url(r'^accounts/profile/', 'calculator.views.profile', name='profile'),
+    url(r'^accounts/register/',
+        RegistrationView.as_view(),
+        name='registration_register'),
+
 )
